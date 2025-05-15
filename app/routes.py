f16cfb5d4decc333241b2c3fd66900d67fb5e60f
Logan Pinel <loganpinel@outlook.com>
2025-05-14 21:29:51 -0400

    created the dashboard page in 'dashboard.html'

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,16 @@ from . import db
 #create the blueprint for the routes
 view = Blueprint('view', __name__)
 
+#create a global list of months for the dashboard
+MONTHS = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+#route for the dashboard
+@view.route('/')
+def dashboard():
+    #call the MONTHS global variable
+    global MONTHS
+    #render the dashboard template and pass the months list and enumerate python function to the template
+    return render_template('dashboard.html', months=MONTHS, enumerate=enumerate)
